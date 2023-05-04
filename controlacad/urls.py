ee0d2e2ddff57bd4acd8c348052097e42d17f5dd
Alejojooo <alejandrofn2204@gmail.com>
2023-05-04 17:28:14 -0600

    Movidas templates a su app correspondiente

--- controlacad/urls.py
+++ controlacad/urls.py
@@ -6,13 +6,14 @@ from Login.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',views.HomePage,name='inicio'),
-    path('',LoginView.as_view(),name='login'),
-    path('logout/',LogoutView.as_view(),name='logout'),
-    path('signup/',SignupView.as_view(),name='signup'),
-    path('reset-password/',PasswordResetView.as_view(),name='reset-password'),
-    path('password-reset-done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('', LoginView.as_view(), name='login'),
+    path('inicio/', views.HomePage, name='inicio'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/', PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('usuario', include('usuarios.urls', namespace='usuarios')),
 ]
-
