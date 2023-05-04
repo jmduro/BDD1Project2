d2c3e2d841697af974a5048f6af32ab0e2f6ea10
Alejojooo <alejandrofn2204@gmail.com>
2023-05-04 16:34:37 -0600

    Formateo en la app login

--- Login/forms.py
+++ Login/forms.py
@@ -2,14 +2,16 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
-User= get_user_model()
+User = get_user_model()
+
+
 class CustomeCreationUserForm(UserCreationForm):
-    class Meta: 
+    class Meta:
         model = User
-        fields= ("username",
-                'nombre',
-                'apellido',
-                'correo',
-                'telefono',
-        )
-        field_classes={'username':UsernameField}
+        fields = ("username",
+                  'nombre',
+                  'apellido',
+                  'correo',
+                  'telefono',
+                  )
+        field_classes = {'username': UsernameField}
