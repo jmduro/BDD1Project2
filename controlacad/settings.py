4684127f49aebf1195917c57022ba05226289042
Alejojooo <alejandrofn2204@gmail.com>
2023-05-04 14:05:00 -0600

    Conector para SQL Server

--- controlacad/settings.py
+++ controlacad/settings.py
@@ -74,10 +74,28 @@ WSGI_APPLICATION = 'controlacad.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # Autenticación de Windows
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'controlacad',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server',
+                    'trusted_connection': 'true',
+        },
     }
+
+    # Autenticación de SQL Server
+    # 'default': {
+    #     'ENGINE': 'mssql',
+    #     'NAME': 'controlacad',
+    #     'USER': 'USER_NAME',
+    #     'PASSWORD': 'PASSWORD',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    #     'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server',
+    #     },
+    # }
 }
 
 
