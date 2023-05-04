cec0099cfacf837ae16535839d1e3e1d07fb7c19
Alejojooo <alejandrofn2204@gmail.com>
2023-05-04 10:42:37 -0600

    Modelos para el módulo de usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    pass
+
+
+class DocumentoIdentificacion(models.Model):
+    nombre = models.CharField(max_length=30)
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+class CertificacionNacimiento(models.Model):
+    libro = models.CharField(max_length=5)
+    acta = models.CharField(max_length=5)
+    folio = models.CharField(max_length=5)
+
+
+class Estudiante(models.Model):
+    usuario = models.OneToOneField(
+        'Usuario', on_delete=models.CASCADE)
+    tipo_documento = models.ForeignKey(
+        'DocumentoIdentificacion', on_delete=models.SET_NULL, null=True)
+    num_identificacion = models.CharField(max_length=30, null=True)
+    nombres = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    carnet = models.CharField(max_length=15)
+    pais = models.ForeignKey('Pais', on_delete=models.SET_NULL, null=True)
+    telefono = models.CharField(max_length=25)
+    fecha_nacimiento = models.DateField()
+    correo_electronico = models.EmailField()
+    certificacion_nacimiento = models.OneToOneField(
+        'CertificacionNacimiento', on_delete=models.SET_NULL, null=True)
+    habilitado = models.BooleanField(default=True)
