d9a35e616391476447a908f77a382e7cacef7093
Alejojooo <alejandrofn2204@gmail.com>
2023-05-05 15:06:06 -0600

    Agregados campos de docente en modelos de pensum

--- pensum/models.py
+++ pensum/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-#from usuarios import models as usuario_model
 
-# Create your models here.
+
 class Carrera(models.Model):
     ciclos = (
         ('Semestral', 'Semestral'),
@@ -30,13 +29,15 @@ class Carrera(models.Model):
     ciclo_academico = models.CharField(choices=ciclos, max_length=100)
     grado_academico = models.CharField(choices=grados, max_length=100)
     jornada = models.CharField(choices=planes, max_length=100)
-    #encargado_area = models.ForeignKey(usuario_model.Docente, on_delete=models.CASCADE)
-    #coordinador_academico = models.ForeignKey(usuario_model.Docente, on_delete=models.CASCADE)
+    encargado_area = models.ForeignKey(
+        'usuarios.Docente', on_delete=models.CASCADE)
+    coordinador_academico = models.ForeignKey(
+        'usuarios.Docente', on_delete=models.CASCADE)
     habilitado = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.nombre}"
-    
+
 
 class Curso(models.Model):
     codigo = models.CharField(max_length=10)
@@ -48,7 +49,8 @@ class Curso(models.Model):
     horas_lab = models.IntegerField(default=0)
     semestres = models.IntegerField(default=2)
     habilitado = models.BooleanField(default=True)
-    
+
+
 class Pensum(models.Model):
     codigo = models.CharField(max_length=4)
     inicio = models.IntegerField(default=2023)
@@ -56,6 +58,5 @@ class Pensum(models.Model):
     proceso = models.CharField(max_length=250)
     ciclos = models.IntegerField(default=2)
     examen = models.IntegerField(default=100)
-    habilitado = models.BooleanField(default=True)
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
-
+    habilitado = models.BooleanField(default=True)
