850f454b91ddc73dcf1c08cef065985c8b25de55
Alejojooo <alejandrofn2204@gmail.com>
2023-05-04 20:39:05 -0600

    Actualizadas vistas

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,13 @@
+from typing import Any, Dict
 from django.shortcuts import render, reverse
 from django.views import generic
+from django.contrib.auth import get_user_model
 
 from .models import Estudiante
 from .forms import EstudianteCreateModelForm, EstudianteUpdateModelForm
+from .util import generate_password
+
+UsuarioAutenticable = get_user_model()
 
 
 class EstudianteListView(generic.ListView):
@@ -20,9 +25,25 @@ class EstudianteDetailView(generic.DetailView):
 class EstudianteCreateView(generic.CreateView):
     template_name = 'estudiantes/estudiante-create.html'
     form_class = EstudianteCreateModelForm
+    usuario_global = ''
+    password_global = ''
 
     def get_success_url(self):
         return reverse('estudiantes:estudiante-list')
+
+    def form_valid(self, form):
+        estudiante = form.save(commit=False)
+        usuario = estudiante.carnet
+        password = self.get_context_data()['password']
+        UsuarioAutenticable.objects.create(usuario=usuario, password=password)
+        estudiante.save()
+        return super(EstudianteCreateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        password = generate_password()
+        context.update({'password': password})
+        return context
 
 
 class EstudianteUpdateView(generic.CreateView):
