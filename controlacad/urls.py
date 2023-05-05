be638344e90d1b0e495de94ef60472dd5e43dfaa
Alejojooo <alejandrofn2204@gmail.com>
2023-05-05 09:11:51 -0600

    Formateo de urls

--- controlacad/urls.py
+++ controlacad/urls.py
@@ -7,30 +7,40 @@ from edificios.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('roles/<int:pk>/editar/',RolUpdate),
-    path('roles/<int:pk>/modificar/',RolModificar),
+    # Roles
+    path('roles/<int:pk>/editar/', RolUpdate),
+    path('roles/<int:pk>/modificar/', RolModificar),
     path('', LoginView.as_view(), name='login'),
-    path('roles/',views.RolPage,name='roles'),
-    path('create-rol/',views.RolCreate,name='create_rol'),
+    path('roles/', views.RolPage, name='roles'),
+    path('create-rol/', views.RolCreate, name='create_rol'),
+    # Login - Signup
     path('inicio/', views.HomePage, name='inicio'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('password-reset-done/', PasswordResetDoneView.as_view(),
          name='password_reset_done'),
-    path('password-reset-complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
     path('password-reset-confirm/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # Usuarios
     path('usuarios/', include('usuarios.urls', namespace='usuarios')),
+    # Edificios
     path('edificio/', EdificioPage, name='edificio_page'),
     path('edificio/crear/', EdificioCrearPage, name='edificio_crear'),
     path('edificio/editar/', EdificioEditarPage, name='edificio_editar'),
     path('edificio/salones/', EdificioSalonesPage, name='edificio_salones'),
-    path('edificio/salones/crear/', EdificioSalonesCrearPage, name='edificio_salones_crear'),
-    path('edificio/salones/editar/', EdificioSalonesEditarPage, name='edificio_salones_editar'),
-    path('edificio/salones/clasificacion', EdificioSalonesClasificacionPage, name='edificio_clasificacionSalones'),
-    path('edificio/salones/clasificacion/crear/', EdificioSalonesClasificacionCrearPage, name='salon_clasificacion_crear'),
-    path('edificio/salones/clasificacion/editar/', EdificioSalonesClasificacionEditarPage, name='salon_clasificacion_editar'),
+    path('edificio/salones/crear/', EdificioSalonesCrearPage,
+         name='edificio_salones_crear'),
+    path('edificio/salones/editar/', EdificioSalonesEditarPage,
+         name='edificio_salones_editar'),
+    path('edificio/salones/clasificacion', EdificioSalonesClasificacionPage,
+         name='edificio_clasificacionSalones'),
+    path('edificio/salones/clasificacion/crear/',
+         EdificioSalonesClasificacionCrearPage, name='salon_clasificacion_crear'),
+    path('edificio/salones/clasificacion/editar/',
+         EdificioSalonesClasificacionEditarPage, name='salon_clasificacion_editar'),
 
 
 ]
