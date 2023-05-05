fd03505876c890de852f456f98d8e0fe3e51cde3
Alejojooo <alejandrofn2204@gmail.com>
2023-05-05 09:08:36 -0600

    Update models.py

--- Login/models.py
+++ Login/models.py
@@ -7,22 +7,19 @@ from django.contrib.auth.models import AbstractUser
 class UsuarioAutenticable(AbstractUser):
     pass
 
+
 class Rol(models.Model):
     nombre = models.CharField(max_length=20)
     descripcion = models.TextField()
-    administrar_carreras=models.BooleanField(default=True)
-    administrar_cursos=models.BooleanField(default=True)
-    administrar_estudiantes=models.BooleanField(default=True)
-    administrar_docentes=models.BooleanField(default=True)
-    administrar_usuarios=models.BooleanField(default=True)
-    validar_cargas_academicas=models.BooleanField(default=True)
-    administrar_roles_y_permisos=models.BooleanField(default=True)
-    administracion_de_edificios_y_salones=models.BooleanField(default=True)
-    habilitado=models.BooleanField(default=True)
-   
+    administrar_carreras = models.BooleanField(default=True)
+    administrar_cursos = models.BooleanField(default=True)
+    administrar_estudiantes = models.BooleanField(default=True)
+    administrar_docentes = models.BooleanField(default=True)
+    administrar_usuarios = models.BooleanField(default=True)
+    validar_cargas_academicas = models.BooleanField(default=True)
+    administrar_roles_y_permisos = models.BooleanField(default=True)
+    administracion_de_edificios_y_salones = models.BooleanField(default=True)
+    habilitado = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.nombre} {self.descripcion} {self.habilitado}"
-
-
-
