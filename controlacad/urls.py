59e9e886af5332adef2caf8a1558c4eaee1273ae
jmduro <jmdubonx@gmail.com>
2023-05-05 01:32:44 -0600

    Model 3 fase 1 terminada
    Html, url, vistas simples

--- controlacad/urls.py
+++ controlacad/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from Login import views
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 from Login.views import SignupView
-from edificios.views import EdificioPage, EdificioView
+from edificios.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +14,16 @@ urlpatterns = [
     path('reset-password/',PasswordResetView.as_view(),name='reset-password'),
     path('password-reset-done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('edificio/', EdificioPage),
-    path('edificio/crear/', EdificioView.as_view(),name='edificio_crear'),
+    path('edificio/', EdificioPage, name='edificio_page'),
+    path('edificio/crear/', EdificioCrearPage, name='edificio_crear'),
+    path('edificio/editar/', EdificioEditarPage, name='edificio_editar'),
+    path('edificio/salones/', EdificioSalonesPage, name='edificio_salones'),
+    path('edificio/salones/crear/', EdificioSalonesCrearPage, name='edificio_salones_crear'),
+    path('edificio/salones/editar/', EdificioSalonesEditarPage, name='edificio_salones_editar'),
+    path('edificio/salones/clasificacion', EdificioSalonesClasificacionPage, name='edificio_clasificacionSalones'),
+    path('edificio/salones/clasificacion/crear/', EdificioSalonesClasificacionCrearPage, name='salon_clasificacion_crear'),
+    path('edificio/salones/clasificacion/editar/', EdificioSalonesClasificacionEditarPage, name='salon_clasificacion_editar'),
+
+
 ]
 
--- edificios/views.py
+++ edificios/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+#Validar esta view para
 class EdificioView(generic.CreateView):
     template_name = "edificio/edificioCrear.html"
     form_class = CustomeCreationEdificioForm
@@ -15,3 +16,27 @@ class EdificioView(generic.CreateView):
 
 def EdificioPage(request):
     return render(request, 'edificio/edificio.html')
+
+def EdificioCrearPage(request):
+    return render(request, 'edificio/edificioCrear.html')
+
+def EdificioEditarPage(request):
+    return render(request, 'edificio/edificioEditar.html')
+
+def EdificioSalonesPage(request):
+    return render(request, 'edificio/salones/salones.html')
+
+def EdificioSalonesCrearPage(request):
+    return render(request, 'edificio/salones/salonesCrear.html')
+
+def EdificioSalonesEditarPage(request):
+    return render(request, 'edificio/salones/salonesEditar.html')
+
+def EdificioSalonesClasificacionPage(request):
+    return render(request, 'edificio/salones/salonesClasificacion.html')
+
+def EdificioSalonesClasificacionCrearPage(request):
+    return render(request, 'edificio/salones/salonesClasificacionCrear.html')
+
+def EdificioSalonesClasificacionEditarPage(request):
+    return render(request, 'edificio/salones/salonesClasificacionEditar.html')
