b5b5185d94e4487151174cd87711c5baf8320b19
Oscar Sierra <62762176+darth7ghost@users.noreply.github.com>
2023-05-05 14:52:18 -0600

    Create pensum
    Apartado de creación de pensum específico para cada carrera, además de apartado de editar pensum.

--- pensum/admin.py
+++ pensum/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Carrera, Curso
+from .models import Carrera, Curso, Pensum
 
 # Register your models here.
 admin.site.register(Carrera)
 admin.site.register(Curso)
+admin.site.register(Pensum)
