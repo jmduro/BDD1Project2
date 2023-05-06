50a26cdbffc636704c043b5851b5e533f304e880
Alejojooo <alejandrofn2204@gmail.com>
2023-05-05 19:55:40 -0600

    Conexión a SQL Server

--- controlacad/settings.py
+++ controlacad/settings.py
@@ -77,12 +77,37 @@ WSGI_APPLICATION = 'controlacad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+
+# }
+
 DATABASES = {
+    # Autenticación de Windows
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'controlacad',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server',
+                    'trusted_connection': 'true',
+        },
     }
 
+    # Autenticación de SQL Server
+    # 'default': {
+    #     'ENGINE': 'mssql',
+    #     'NAME': 'controlacad',
+    #     'USER': 'USER_NAME',
+    #     'PASSWORD': 'PASSWORD',
+    #     'HOST': 'localhost',
+    #     'PORT': '',
+    #     'OPTIONS': {'driver': 'ODBC Driver 17 for SQL Server',
+    #     },
+    # }
 }
 
 
